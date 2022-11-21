7a8d72e76b1afbb68139b3191ea8a6b81b7fad83
mckaraman <mustafacankaramann@gmail.com>
2022-11-22 01:29:55 +0300

    Üçgen Alan Hesaplama

--- src/hipotenusbulma.java
+++ src/hipotenusbulma.java
@@ -3,11 +3,11 @@ public class hipotenusbulma {
     public static void main(String[]args){
         double a,b,c,u,alan;
         Scanner input = new Scanner(System.in);
-        System.out.print("1. Kenarı Giriniz: ");
+        System.out.print("1. Kenarı Giriniz : ");
         a = input.nextDouble();
         System.out.print("2. Kenarı Giriniz : ");
         b = input.nextDouble();
-        System.out.print("3. Kenarı Giriniz: ");
+        System.out.print("3. Kenarı Giriniz : ");
         c = input.nextDouble();
 
         u = (a+b+c)/2;
